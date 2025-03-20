19d42edd76a181f8b5764ff9b2abefb3d98bf0db
Cesar Giraldo <cesargica1@gmail.com>
2025-03-19 21:51:02 -0500

    Add files via upload

---
+++ taller de estructuras repeticion/ejercicio1.py
@@ -0,0 +1,3 @@
+numero=int(input("ingrese numero a multiplicar"))
+for i in range(1,11,1):
+    print(f"numero {numero} x {i} = {numero*i} ")
---
+++ taller de estructuras repeticion/ejercicio3.py
@@ -0,0 +1,7 @@
+suma=0
+for i in(97,1004,1):
+    if(i%2==0):
+        suma=suma+i
+        print(suma)
+        
+    
